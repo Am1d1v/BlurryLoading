fcd43b2b4d1b0c8bcba47eef0f3f52c04e1c54b5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-17 15:31:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 
 
 
+const loadingText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+
+let interval = setInterval(() => {
+    Blurring();
+}, 30)
+
+
+function Blurring(){
+    load++;
+
+    // Loading increases until it has value 100
+    if(load > 99){
+        clearInterval(interval);
+    }
+
+
+    loadingText.innerHTML = `${load}%`;
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+    
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+}
